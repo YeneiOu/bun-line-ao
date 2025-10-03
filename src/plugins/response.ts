cc4f45cc010629f66cb0d270e36c91dd09f0db14
YeneiOu <ei.yen_chen@hotmail.com>
2025-10-03 12:48:07 +0700

    fix: response

--- src/plugins/response.ts
+++ src/plugins/response.ts
@@ -1,14 +1,17 @@
-import { success, error } from "../utils/response"
+import { ResponseSuccess, ResponseError } from "../utils/response";
 // @ts-ignore
-import { Elysia } from "elysia"
+import { Elysia } from "elysia";
 
 export const responsePlugin = (app: Elysia) =>
   app.derive(() => ({
-    resSuccess: <T = any>(data?: T, message: string = "success", code: number = 0) =>
-      success(code, message, data),
+    resSuccess: <T = any>(
+      data?: T,
+      message: string = "success",
+      code: number = 0,
+    ) => ResponseSuccess(code, message, data),
     resError: <T = any>(
       message: string,
       code: number = 1,
-      data: T | null = null
-    ) => error(code, message, data)
-  }))
+      data: T | null = null,
+    ) => ResponseError(code, message, data),
+  }));
